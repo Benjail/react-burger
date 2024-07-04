9e5448f19b1c493714a3642b9fca0f47741a4b04
Mikhail Yarlykov <miha203@mail.ru>
2024-07-05 02:35:22 +0300

    Final fixes

--- src/copmonents/burger-constructor/burger-constructor.tsx
+++ src/copmonents/burger-constructor/burger-constructor.tsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useState, useEffect } from 'react';
+import { 
+  ConstructorElement,
+  DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import Modal from '../modal/modal';
 import OrderDetails from './order-details/order-details';
 import BurgerConstructorInfo from './constructor-info/constructor-info';
@@ -10,7 +12,20 @@ import { ingredientPropType } from '../..//utils/types';
 // @ts-ignore: suppress implicit any error
 function BurgerConstructor({ingredients}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
+  const [bunIngredient, setBunIngredient] = useState(null);
+  const [otherIngredients, setOtherIngredients] = useState([]);
+
+  useEffect(()=>{
+    setBunIngredient(
+      // @ts-ignore: suppress implicit any error
+      ingredients?.find(({ type }) => type === "bun") || null
+    );
+    setOtherIngredients(
+      // @ts-ignore: suppress implicit any error
+      ingredients?.filter(({ type }) => type !== "bun")
+    );
+  }, [ingredients]);
+
   const handleOrderClick = () => { 
     setIsModalOpen(true);
   };
@@ -18,26 +33,53 @@ function BurgerConstructor({ingredients}) {
     setIsModalOpen(false);
   };
 
-  
+
   return (
      <div className={styles.burger_constructor}>
-        <div className={styles.burger_constructor_container}>
-        {ingredients.map((ingredient:any, index:any) => (
-        <div className={styles.burger_constructor_item} key={ingredient._id}>
+        <div className='mt-25'>
+          {bunIngredient && (
           <ConstructorElement
-          key={index}
-          type={
-            index === 0 ? 'top' : 
-            index === ingredients.length - 1 ? 'bottom' : undefined
-          }
-            text={ingredient.name}
-            price={ingredient.price}
-            thumbnail={ingredient.image}
+            type="top"
+              // @ts-ignore: suppress implicit any error
+            text={`${bunIngredient.name} (вверх)`}
+              // @ts-ignore: suppress implicit any error
+            price={bunIngredient.price}
+              // @ts-ignore: suppress implicit any error
+            thumbnail={bunIngredient.image}
+            isLocked={true}
           />
+          )}
         </div>
-      ))}
-    </div>
-    
+        <div className={styles.burger_constructor_container}>
+          {otherIngredients.map((ingredient:any, index:any) => (
+          <div className={styles.burger_constructor_item} key={ingredient._id}>
+            {/* {!(index === 0 || index === ingredients.length - 1) && (
+                <DragIcon type='primary' />
+              )} */}
+            <DragIcon type='primary' />
+            <ConstructorElement
+              key={index}
+              text={ingredient.name}
+              price={ingredient.price}
+              thumbnail={ingredient.image} 
+            />
+          </div>
+          ))}
+        </div>
+        <div>
+          {bunIngredient && (
+          <ConstructorElement
+            type="bottom"
+              // @ts-ignore: suppress implicit any error
+            text={`${bunIngredient.name} (вниз)`}
+              // @ts-ignore: suppress implicit any errors
+            price={bunIngredient.price}
+              // @ts-ignore: suppress implicit any error
+            thumbnail={bunIngredient.image}
+            isLocked={true}
+          />
+          )}
+        </div>
     <BurgerConstructorInfo onOrderClick={handleOrderClick} />
     {isModalOpen && <Modal onClose={handleCloseModal} >
          <OrderDetails/>
